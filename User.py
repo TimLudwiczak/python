a6679dfc1a8642fe1a20b3af0d918b51707d11eb
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-10 20:32:48 -0500

    added pytest

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, name, email, drivers_license, number):
+        self.name = name
+        self.email = email
+        self.drivers_license = drivers_license
+        self.number = number
+user1 = User('Billy', 'billybob@gmail.com', 'YWX345', '123-555-1234')  
+user2 = User('James', 'jamesbond@forreal.edu', 'none', '555-100-0126')
--- user_class.py
+++
@@ -1,6 +0,0 @@
-class user:
-    def __init__(self, name, email, drivers_license, number):
-        self.name = name
-        self.email = email
-        self.drivers_license = drivers_license
-        self.number = number
